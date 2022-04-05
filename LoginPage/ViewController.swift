8819db9c891fbfd17770417fd226e596a5ffafdb
seregious <seregious@gmail.com>
2022-04-05 16:42:01 +0300

    Initial Commit

---
+++ LoginPage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginPage
+//
+//  Created by Сергей Иванчихин on 05.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
